c6b1106796e050cb034a243daebcbda257fedbd2
Georgekhvichia1 <giokhvichia1@gmail.com>
2024-02-02 15:33:43 +0400

    planets

---
+++ README.md
@@ -0,0 +1,49 @@
+# Galaxy Explorer 
+
+
+
+## Description
+
+Galaxy Explorer is a web application that provides information about planets in our galaxy. It fetches data from Wikipedia using an API and displays details such as rotation time, revolution time, radius, and average temperature for each planet.
+# Open the Project
+for Windows:
+- `start index.html`
+
+for Linux
+- `xdg-open index.html`
+
+for macOS
+- `open index.html`
+
+To start exploring the project, simply double-click on the "index.html" file in the project's folder. Once you've done that, you're ready to dive in and interact with the project using your web browser.
+
+
+Open the **index.html**  file in your preferred web browser.
+## Usage
+ - Open the application in your web browser.
+ - Select a planet to view its specifications.
+ 
+
+## Technologies Used
+ - HTML
+ - CSS
+ - JavaScript
+
+## Features
+
+- View detailed information about planets in our galaxy.
+- Interactive user interface with HTML, CSS, and JavaScript.
+- Data fetched from Wikipedia using an API.
+
+
+## Installation
+
+1. Clone the repository:
+
+git clone
+
+
+
+## Author
+
+This project was created by **GIORGI KHVICHIA**
---
+++ main.js
@@ -0,0 +1,74 @@
+const planetLinks = document.querySelectorAll('.planet');
+const planetTitle = document.querySelector('#title');
+const  planetImg = document.querySelector('#planet-img');
+const planetImgAdd = document.querySelector('#planet-imgadd');
+const overviewFirst = document.querySelector('#overview');
+const internalSec = document.querySelector('#internal');
+const geologyThird = document.querySelector('#geology');
+const infoP = document.querySelector('#info-p');
+const rotationN = document.querySelector('#rotation');
+const revolutionN = document.querySelector('#revolution');
+const radiusN = document.querySelector('#radius');
+const temperatureN = document.querySelector('#temperature');
+const burgerBtn = document.querySelector('#burger-btn');
+const planetBtn = document.querySelector('#planet-btn');
+
+
+
+const PLANET_API = 'https://planets-api.vercel.app/api/v1/planets';
+   
+for (let i = 0; i <  planetLinks.length; i++) {
+    planetLinks[i].addEventListener('click', () => {
+        selectPlanet(planetLinks[i].textContent);
+    });
+}
+
+
+
+const selectPlanet = async (planet = 'Mercury') => {
+    const response = await fetch(`${PLANET_API}/${planet}`);
+    const data = await response.json();
+    planetTitle.textContent = data.name;
+    planetImg.src = data.images.planet;
+    infoP.textContent = data.overview.content;
+    planetImgAdd.style.display = 'none';
+    rotationN.textContent = data.rotation;
+    revolutionN.textContent = data.revolution;
+    radiusN.textContent = data.radius;
+    temperatureN.textContent = data.temperature;
+
+
+
+   
+
+    overviewFirst.addEventListener('click', () => {
+        infoP.textContent = data.overview.content;
+        planetImg.src = data.images.planet;
+        planetImgAdd.style.display = 'none';
+    });
+
+    internalSec.addEventListener('click', () => {
+        infoP.textContent = data.structure.content;
+        planetImg.src = data.images.internal;
+        planetImgAdd.style.display = 'none';
+    });
+
+    geologyThird.addEventListener('click', () => {
+        infoP.textContent = data.geology.content;
+        planetImg.src = data.images.planet;
+        planetImgAdd.src = data.images.geology;
+        planetImgAdd.style.display = 'block';  
+    });
+
+};
+
+selectPlanet();
+
+
+burgerBtn.addEventListener('click', () => {
+    planetBtn.classList.toggle('active');
+    burgerBtn.classList.toggle('active');
+});
+
+
+   
